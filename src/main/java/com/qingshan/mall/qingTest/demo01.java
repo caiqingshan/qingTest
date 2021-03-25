7bafd7c4399bff28fb0d7044ab7b8a4ac562404e
CaiQingshan <caiqingshan01@163.com>
2021-03-25 17:51:59 +0800

    第二次改变02

--- src/main/java/com/qingshan/mall/qingTest/demo01.java
+++ src/main/java/com/qingshan/mall/qingTest/demo01.java
@@ -11,13 +11,13 @@ import java.util.stream.Collectors;
 
 public class demo01 {
     public static void main(String[] args) throws ParseException {
-        System.out.println("1111111111111111111");
-        System.out.println("2222222222222222222");
-        System.out.println("3333333333333333333");
-        System.out.println("4444444444444444444");
-        System.out.println("5555555555555555555");
-        System.out.println("6666666666666666666");
-        System.out.println("7777777777777777777");
+        System.out.println("qqqqqqqqqqqqqqqqqq");
+        System.out.println("wwwwwwwwwwwwwwwwwww");
+        System.out.println("eeeeeeeeeeeeeeee");
+        System.out.println("rrrrrrrrrrrrrrrrrrrr");
+        System.out.println("ttttttttttttttttttttt");
+        System.out.println("yyyyyyyyyyyyyyyyy");
+        System.out.println("uuuuuuuuuuuuuuuuuuu");
 
     }
 }
