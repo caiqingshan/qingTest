0acaf18e87d21a5885adeb88b850faaf64ea0a47
CaiQingshan <caiqingshan01@163.com>
2021-03-25 22:37:23 +0800

    修改dev

--- src/main/java/com/qingshan/mall/qingTest/demo01.java
+++ src/main/java/com/qingshan/mall/qingTest/demo01.java
@@ -34,5 +34,6 @@ public class demo01 {
         System.out.println("-----------------------------");
         System.out.println("******************************");
         System.out.println("++++++++++++++++++++++++++++++");
+        System.out.println("caiqingshancaiqingshancaiqingshancaiqingshancaiqinagshancaiqiangshancaiqingshan");
     }
 }
