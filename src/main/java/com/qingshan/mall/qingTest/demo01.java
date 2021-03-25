993bcdea839fe7f1798ae6bcc223e26efcf5cd95
CaiQingshan <caiqingshan01@163.com>
2021-03-25 18:48:44 +0800

    改变test

--- src/main/java/com/qingshan/mall/qingTest/demo01.java
+++ src/main/java/com/qingshan/mall/qingTest/demo01.java
@@ -11,17 +11,16 @@ import java.util.stream.Collectors;
 
 public class demo01 {
     public static void main(String[] args) throws ParseException {
-        System.out.println("111111111111111");
-        System.out.println("112222222222222222222");
-        System.out.println("333333333333333");
-        System.out.println("3334444444444444444444");
-        System.out.println("*************");
+        System.out.println("@@@@@@@@@@@@@@");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("22222222222");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@*****");
         System.out.println("787878787878787887878787878787878");
-        System.out.println("111111111111111111111111111111111");
-        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
-        System.out.println("/////////////////////////////");
-        System.out.println("-----------------------------");
-        System.out.println("******************************");
-        System.out.println("++++++++++++++++++++++++++++++");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@!!!!!!!!!!");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@!!!!!!!!!!!!");
+        System.out.println("@@@@@@@@@@@@@@@@@@@!!!!!!!!!!!!!");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@!!!!!!!!!!");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@!!!!!!!!!!!!!");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!!!!!!!");
     }
 }
